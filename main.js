13e810b3ab5fa6159c3412a26d0af7cca32e292f
Evandro Junior <47798920+evandrmb@users.noreply.github.com>
2021-07-03 19:28:49 -0300

    Added comments to main.js

--- main.js
+++ main.js
@@ -16,16 +16,16 @@ const getCurrentColor = () => {
   }
 };
 
-const getCssBackgroundColor = (key) => {
-  return `linear-gradient(45deg,var(--${Colors[key]}),var(--light-${Colors[key]}))`;
-};
+const getCssBackgroundColor = (key) =>
+  `linear-gradient(45deg,var(--${Colors[key]}),var(--light-${Colors[key]}))`;
 
 const updateElementsColor = (currentColor) => {
   const elementsWithColor = document.querySelectorAll(".selected-color");
   const colorSelected = document.querySelector(".colors__item--selected");
 
+  // Updates below banner color allowing the effect
+  // of sliding the new selected color
   const belowBanner = document.querySelector(".below-banner");
-
   belowBanner.style.setProperty(
     "background-image",
     getCssBackgroundColor(currentColor)
@@ -33,6 +33,11 @@ const updateElementsColor = (currentColor) => {
 
   for (const key in Colors) {
     if (colorSelected.classList.contains(Colors[key])) {
+      // Updates the image based on Color
+      const img = document.querySelector(".shoes");
+      img.src = `assets/img/${Colors[key]}.png`;
+
+      //updates the color of each element in `elementsWithColor`
       for (const element of elementsWithColor) {
         if (
           Colors[key] === "blue" ||
@@ -51,10 +56,11 @@ const updateElementsColor = (currentColor) => {
         }
       }
 
+      // Updates the banner color
       const banner = document.querySelector(".banner");
-
       banner.style.setProperty("background-image", getCssBackgroundColor(key));
 
+      // restart the animation
       if (currentColor !== key) {
         banner.classList.add("change-color-animation");
         banner.style.animation = "none";
